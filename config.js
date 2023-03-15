16639831d59bc8d4ed70dfed3bc9b3c4229eaf49
erenflex <127881447+erenflex@users.noreply.github.com>
2023-03-15 14:04:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-  TOKEN: "MTA4NDkxNDgzNDAxNTAwMjYzNA.GHWM5d.w9xt1pzFiybK1EVLqcybQPWiMSrqmVUWJFe9hA",
-  authorID: "1082725238686634095", //write your discord account id.
-  url: "atatv44", //Type the Custom Discord Server URL You Want to Retrieve!
-  serverID: "1070640954912014397", //Type the Server ID to Get the Url!
-  log: "1084881480213340280", //If the URL is taken, write the Log Channel ID to be informed!
+  TOKEN: "",
+  authorID: "", //write your discord account id.
+  url: "", //Type the Custom Discord Server URL You Want to Retrieve!
+  serverID: "", //Type the Server ID to Get the Url!
+  log: "", //If the URL is taken, write the Log Channel ID to be informed!
   status: "true", //false=Stop Bot | true=Means Start the Bot!
   botRun: "100" //1000 = 1 Second | 1 = 1 Millisecond | It is recommended to write at least 50 milliseconds. Under 30MS, the project will be banned from the Discord API and will stop working!
 
